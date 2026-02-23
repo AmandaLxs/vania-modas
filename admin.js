0d9fb55a64d401c3d818ff1dd96c74e865f27323
Amanda Lima <amandalima.181@icloud.com>
2026-02-23 10:57:47 -0300

    Add salvar function to register new product

--- admin.js
+++ admin.js
@@ -1,1 +1,13 @@
+function salvar(){
 
+const nome = document.getElementById("nome").value
+const preco = Number(document.getElementById("preco").value)
+
+db.collection("produtos").add({
+nome,
+preco,
+criado: new Date()
+})
+
+alert("Produto cadastrado!")
+}
